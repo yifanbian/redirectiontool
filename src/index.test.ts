c04f7ab03ea4cc0d239ba7c49e3fe9f653db5224
Yifan Bian <bianyifan@users.noreply.github.com>
2024-09-15 05:49:35 +0000

    bump

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,15 @@
-import fs from 'fs/promises';
-import os from 'os';
-import path from 'path';
-import { KVNamespace } from "@miniflare/kv";
-import { FileStorage } from "@miniflare/storage-file";
-import { unstable_dev } from 'wrangler';
-import type { UnstableDevWorker } from 'wrangler';
-import { describe, expect, it, beforeAll, afterAll } from 'vitest';
+import { describe, expect, it, beforeAll } from 'vitest';
+
+import { Miniflare } from "miniflare";
+
+const kvNamespaceName = 'redirectiontool'; // must be aligned with kv namespace binding in wrangler.toml
+const mf = new Miniflare({
+  modules: true,
+  scriptPath: 'dist/node.js',
+  kvNamespaces: [kvNamespaceName],
+});
 
 describe('handler returns response with request method', () => {
-  const kvNamespaceName = 'redirectiontool'; // must be aligned with kv namespace binding in wrangler.toml
-  let tempDir: string;
-  let worker: UnstableDevWorker;
   const routes: [string, string | null][] = [
     ['/', 'https://www.google.com/'],
     ['/msft', 'https://www.microsoft.com/'],
@@ -18,27 +17,12 @@ describe('handler returns response with request method', () => {
   ];
 
   beforeAll(async () => {
-    tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'cfw-'));
-    console.log(tempDir);
-
-    const ns = new KVNamespace(new FileStorage(path.join(tempDir, 'kv', kvNamespaceName)));
+    const ns = await mf.getKVNamespace(kvNamespaceName);
     await Promise.all(routes.map(async ([source, target]) => {
       if (target) {
         await ns.put(source, target);
       }
     }));
-
-    worker = await unstable_dev('src/index.ts', {
-      experimental: { disableExperimentalWarning: true },
-      local: true,
-      persist: true,
-      persistTo: tempDir,
-    });
-  });
-
-  afterAll(async () => {
-    await worker.stop();
-    await fs.rm(tempDir, { recursive: true, force: true });
   });
 
   const otherMethods = [
@@ -51,7 +35,7 @@ describe('handler returns response with request method', () => {
   ];
   routes.forEach(([route, expectedResult]) => {
     it(`GET ${route}`, async () => {
-      const result = await worker.fetch(route, {
+      const result = await mf.dispatchFetch('https://localhost' + route, {
         method: 'GET',
         redirect: 'manual',
       });
@@ -65,7 +49,7 @@ describe('handler returns response with request method', () => {
 
     otherMethods.forEach((method) => {
       it(`${method} ${route}`, async () => {
-        const result = await worker.fetch(route, {
+        const result = await mf.dispatchFetch('https://localhost' + route, {
           method,
           redirect: 'manual',
         });
